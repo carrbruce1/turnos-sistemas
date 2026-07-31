ac06dd7183fa1dcbd05df3dfee2f5db36d319d53
carrbruce1 <brucecarr1290@hotmail.com>
2026-07-31 15:41:46 -0300

    modi en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,33 @@
 import { Routes } from '@angular/router';
-import { HomeComponent}  from './pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { LoginComponent } from './pages/login/login.component';
 import { EmpleadosComponent } from './pages/empleados/empleados.component';
 
 export const routes: Routes = [
-
-    {
-        path: "Home",
-        component: HomeComponent,
-    },
-    {
-        path: "login",
-        component: LoginComponent,
-    },
-    {
-        path: "empleados",
-        component: EmpleadosComponent,
-    },
-    {
-        path: "adminPanel",
-        component: AdminComponent,
-    },
-    {
-
-        path: '',
-        redirectTo: "Home",
-        pathMatch: "full",
-    },
-    {
-        path: '**',
-        redirectTo: "Home",
-    }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home', // En minúscula para evitar problemas de matching
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'empleados',
+    component: EmpleadosComponent,
+  },
+  {
+    path: 'adminPanel',
+    component: AdminComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
 ];

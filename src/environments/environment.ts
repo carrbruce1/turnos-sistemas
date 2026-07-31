d82af4ac31abf79851a2685cad491f1e1923a400
carrbruce1 <brucecarr1290@hotmail.com>
2026-07-31 16:01:42 -0300

    modi en env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  supabaseKey: 'https://fyemmozlzywnfcsyamqn.supabase.co',
-  supabaseUrl: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5ZW1tb3psenl3bmZjc3lhbXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NTA5MTQsImV4cCI6MjEwMTAyNjkxNH0.bB0HnHs5ZzYzSROPTuFL9jsPdZ3PQs4RjwRVv3ZTrEE'
+  production: true,
+  supabaseUrl: 'https://fyemmozlzywnfcsyamqn.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5ZW1tb3psenl3bmZjc3lhbXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NTA5MTQsImV4cCI6MjEwMTAyNjkxNH0.bB0HnHs5ZzYzSROPTuFL9jsPdZ3PQs4RjwRVv3ZTrEE'
 };

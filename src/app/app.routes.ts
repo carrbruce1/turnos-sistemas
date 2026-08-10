7a80619fc67bc9de40ab744a9d89c0e424dbfcbe
carrbruce1 <brucecarr1290@hotmail.com>
2026-08-10 01:19:00 -0300

    modi app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { LoginComponent } from './pages/login/login.component';
 import { EmpleadosComponent } from './pages/empleados/empleados.component';
-import { CrearUsuarioComponent } from './pages/admin/crear-usuario/crear-usuario.component';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
@@ -28,17 +27,19 @@ export const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
-    canActivate:[authGuard],
+    canActivate: [authGuard],
   },
+
+  //uwuuuuuuuuuuuuuuuuuuuuuu
   {
-    path: "crear-usuario",
+    path: 'crear-usuario',
     loadComponent: () => import('./pages/admin/crear-usuario/crear-usuario.component').then(m => m.CrearUsuarioComponent),
     canActivate: [authGuard],
   },
   {
-  path: 'cancelar-turno/:id',
-  loadComponent: () => import('./pages/cancelar-turnos/cancelar-turnos.component').then(m => m.CancelarTurnoComponent)
-},
+    path: 'cancelar-turno/:id',
+    loadComponent: () => import('./pages/cancelar-turnos/cancelar-turnos.component').then(m => m.CancelarTurnosComponent)
+  },
   {
     path: '**',
     redirectTo: 'home',
